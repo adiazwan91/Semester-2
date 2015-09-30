9a7c360f53957eead80ef8dd52b8eb8bbf8150c7
adiazwan91 <adiazwan91@gmail.com>
2015-10-01 03:24:12 +0800

    Create TriangleTest.java

---
+++ Object-Oriented-Programming-Java/Triangle/TriangleTest.java
@@ -0,0 +1,23 @@
+package Triangle;
+
+import java.util.*;
+
+public class TriangleTest 
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        
+        Triangle t1 = new Triangle();
+        
+        System.out.println("Please enter side 1: ");
+        t1.side1 = input.nextDouble();
+        System.out.println("Please enter side 2: ");
+        t1.side2 = input.nextDouble();
+        System.out.println("Please enter side 3: ");
+        t1.side3 = input.nextDouble();
+        
+        System.out.println("The area of triangle: "+t1.calculateArea());
+        System.out.println("The perimeter of triangle: "+t1.calculatePerimeter());
+    }
+}
