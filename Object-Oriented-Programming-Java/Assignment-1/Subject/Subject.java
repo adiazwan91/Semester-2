974aae2fa15b7a7dedbe43e9fc1d1b279c38ca95
adiazwan91 <adiazwan91@gmail.com>
2015-10-15 00:12:45 +0800

    Update Subject.java

--- Object-Oriented-Programming-Java/Assignment-1/Subject/Subject.java
+++ Object-Oriented-Programming-Java/Assignment-1/Subject/Subject.java
@@ -5,8 +5,8 @@ import java.util.*;
 public class Subject 
 {
     private String subjectName;
-    private static String[] arrStudent = new String[10];
-    private static int studentNum;
+    private String[] arrStudent = new String[100];
+    private int studentNum;
     
     public Subject()
     {
@@ -20,9 +20,34 @@ public class Subject
         this.subjectName = subjectName;
     }
     
-    public void addStudent(String[] arrStudent)
+    public void addStudent(int counter)
     {
-       
+       Scanner input = new Scanner(System.in);
+       for(int i = 0; i < counter; i++)
+       {
+           int choose;
+           System.out.println("Please enter subject name: 1 for Database Systems and 2 for Object-Oriented Programming");
+           choose = input.nextInt();
+  
+           if(choose == 1)
+           {
+            subjectName = "Database Systems";
+            System.out.println("Please input student name: ");
+            arrStudent[i] = input.next();
+            studentNum = studentNum + 1;
+           }
+           
+           else if(choose == 2)
+           {
+            subjectName = "Object-Oriented Programming";
+            System.out.println("Please input student name: ");
+            arrStudent[i] = input.next();
+            studentNum = studentNum + 1;
+           }
+           else 
+               System.exit(0);
+           
+        }
     }
     
     public String getSubjectName()
@@ -38,50 +63,5 @@ public class Subject
     public int getStudentNum()
     {
         return studentNum;
-    }
-
-    public static void main(String[] args)
-    {
-       Subject dbs = new Subject("Database Systems");
-       Subject oop = new Subject("Object Oriented Programming");
-       
-       Scanner input = new Scanner(System.in);
-       int choose;
-       
-       System.out.println("Choose 1 for DB and 2 for OOP");
-       choose = input.nextInt();
-       
-       if(choose == 1)
-        {
-            System.out.println("Please enter how many student");
-            studentNum = input.nextInt();
-       
-                for(int i = 0; i < studentNum; i++)
-                {
-                    System.out.println("Please enter name");
-                    arrStudent[i] = input.next();
-                }
-        }
-       
-       else if(choose == 2)
-       {
-            System.out.println("Please enter how many student");
-            studentNum = input.nextInt();
-       
-                for(int j = 0; j < studentNum; j++)
-                {
-                    System.out.println("Please enter name");
-                    arrStudent[j] = input.next();
-                }
-       }
-       else
-           System.exit(0);
-       
-       System.out.println("Subject Name:"+dbs.getSubjectName());
-       System.out.println("Student Name:"+Arrays.toString(dbs.getArrStudent()));
-       System.out.println("Student Number:"+dbs.getStudentNum());
-       System.out.println("Subject Name:"+oop.getSubjectName());        
-       System.out.println("Student Name:"+Arrays.toString(oop.getArrStudent()));
-       System.out.println("Student Number:"+oop.getStudentNum());
     }
 }
