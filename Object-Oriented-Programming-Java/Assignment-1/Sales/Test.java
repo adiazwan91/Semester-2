c49eb2f9842ff9102e0eca4d99f315fc0330b3a1
adiazwan91 <adiazwan91@gmail.com>
2015-10-14 22:18:43 +0800

    Update Test.java

--- Object-Oriented-Programming-Java/Assignment-1/Sales/Test.java
+++ Object-Oriented-Programming-Java/Assignment-1/Sales/Test.java
@@ -2,31 +2,40 @@ package Sales;
 
 import java.util.*;
 
-public class Test 
+public class Test //Test class
 {
-    public static void main(String[] args)
+    public static void main(String[] args) //Main method
     {   
-        Scanner input = new Scanner(System.in);
-        Sales s1 = new Sales();
-        Sales s2 = new Sales("SALE7003", 400);
-        Sales s3 = new Sales();
-       
+        Scanner input = new Scanner(System.in); //User input
+        Sales s1 = new Sales("SALE6003", 800); //Create object s1
+        Sales s2 = new Sales("SALE7003", 400); //Create object s2
         
-        int[] salesArray = new int[4];
-        String[] idArray = new String[4];
-        double[] amountArray = new double[4];
+        s2.setSalesId("SALE5003"); //Set s2 sales id to new value
+        s2.setAmount(500); //Set s2 amount to new value
+    
+        s1.displaySalesObject(s1.getSalesId(), s1.getAmount()); //Display s1 value
+        System.out.println("The discount is: RM"+s1.determineDiscount(s1.getAmount())+"\n"); //Display discount for s1
+        s2.displaySalesObject(s2.getSalesId(), s2.getAmount()); //Display s2 value
+        System.out.println("The discount is: RM"+s2.determineDiscount(s2.getAmount())+"\n"); //Display discount for s2
         
-        s1.displaySalesObject("SALE6003", 800);
-        s2.displaySalesObject("SALE5003", 500);
-      
-        for(int i = 0; i < salesArray.length; i++)
+        Sales[] salesArray = new Sales[4]; //Create an array of object
+        
+        for(int i = 0; i < salesArray.length; i++) //Loop for array input
         {
-            System.out.println("Sale id");
-            idArray[i] = input.next();
+            System.out.println("Input sales ID: ");
+            String salesId = input.next(); //Input sales id
       
-            System.out.println("amount");
-            amountArray[i] = input.nextDouble();     
+            System.out.println("Input the amount of sales: ");
+            double amount = input.nextDouble(); //Input amount
+           
+            salesArray[i] = new Sales(); //Create new sales
+            salesArray[i].setSalesId(salesId); //Set sales id value to setSalesId method
+            salesArray[i].setAmount(amount); //Set amount value to setAmount method
         }
-        s3.displaySalesArray(idArray, amountArray);
+        
+        for (Sales salesArray1 : salesArray) //Loop for array output
+        {
+            salesArray1.displaySalesArray(); //Call displaySalesArray method to output
+        }
     }
 }
